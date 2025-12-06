fcfc786af8f685832f728be0ae9efda76a62f4e6
syd-nguyen <syd.tl.nguyen@gmail.com>
2025-12-05 21:46:37 -0500

    added port 8080

--- app.py
+++ app.py
@@ -186,4 +186,4 @@ def getEventCars(eventId, driverName):
     return getEvent(eventId)['cars']
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="8080", debug=True)
