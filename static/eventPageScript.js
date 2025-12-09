0391d7da9b94aa226c98eec127c66a43f0f4dcab
syd-nguyen <syd.tl.nguyen@gmail.com>
2025-12-08 22:14:35 -0500

    added nocors again

--- static/eventPageScript.js
+++ static/eventPageScript.js
@@ -30,6 +30,7 @@ addCarForm.addEventListener("submit", async (e) => {
         const res = await fetch(endpoint, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
+            mode: "no-cors",
             body: JSON.stringify({ driverName: driverName, numberSeats: numberSeats, takenSeats: 0 })
         });
 
